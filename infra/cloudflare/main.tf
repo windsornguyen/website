a2bd9e1e4de7d922488081fcddaabe9efdb77fa8
Windsor Nguyễn <win@dedaluslabs.ai>
2026-09-01 22:08:19 -0700

    chore(infra): initialize cloudflare terraform (#8)

---
+++ infra/cloudflare/main.tf
@@ -0,0 +1,18 @@
+resource "cloudflare_worker" "website" {
+  account_id = var.cloudflare_account_id
+  name       = var.worker_name
+
+  observability = {
+    enabled = true
+  }
+}
+
+resource "cloudflare_workers_custom_domain" "website" {
+  for_each = var.custom_domains
+
+  account_id = var.cloudflare_account_id
+  hostname   = each.value
+  service    = cloudflare_worker.website.name
+  zone_id    = var.cloudflare_zone_id
+  zone_name  = var.zone_name
+}
